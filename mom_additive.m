051ce3f423b1f59641b949ef8a5d3143acc6dae1
Mohammed Alsubeihi <mohammed.alsubeihi30@gmail.com>
2023-03-07 02:12:50 +0000

    initial creation

---
+++ mom_additive.m
@@ -0,0 +1,79 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% The University of Manchester, United Kingdom
+% 
+% Project: MEng Dissertation
+% Year: 2023
+% MATLAB: R2022b
+% Author(s): Mohammed Alsubeihi (MA)
+% 
+% Last modified:
+% - 2022/03/06, MA: initial creation
+%
+% Purpose: Defines a function handle containing the ode system
+% corresponding to the one-dimensional method of moments.This function
+% differentiates itself by using the solubility and growth rate outlined in
+% the paper by T. Vetter et al.
+% References: 
+% (1) Ramkrishna, D., 2000. Population balances : theory and applications
+% to particulate systems in engineering. Academic Press.
+% (2) Vetter, T., Mazzotti, M., Brozio, J., 2011. Slowing the growth rate
+% of ibuprofen crystals using the polymeric additive pluronic F127. Crystal
+% Growth and Design 11. https://doi.org/10.1021/cg200352u
+% (3) Vetter, T., Mazzotti, M., Brozio, J., 2011. Slowing the growth rate
+% of ibuprofen crystals using the polymeric additive pluronic F127. Crystal
+% Growth and Design 11. https://doi.org/10.1021/cg200352u
+%
+% Input arguments:
+% - y: Column vector containing all the dependent variables of the ode
+% system
+% - solubility: Scalar representing the solubility at a particular
+% temperature
+% - k1: Scalar reperesenting one of the growth rate parameters [m/s] (3).
+% - k2: Scalar representing another one of the growth rate parameters [K]
+% (3).
+% - k3: Scalar representing another one of the growth rate parameter [K^2]
+% (3).
+% p0, p1, p2, p3, p4: scalars representing the parameters needed for the
+% solubility polynomial [-] (4).
+% - shapeFactor: Factor that relates the m21 cross-moment with the particle
+% volume
+% - particleDensity: Scalar representing the density of the crystal
+% particles
+%
+% Output arguments:
+% - ddt: system of ODEs corresponding to the two-dimensional method of
+% moments
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function ddt = mom_additive(t_mom, y, temperatureRamp, k1, k2, k3, p0, p1, p2, p3, p4, particleDensity, ...
+    shapeFactor, operationMode)
+%% Store variables to be solved
+
+m0 = y(1);
+m1 = y(2);
+m2 = y(3);
+m3 = y(4);
+concentration_mom = y(5);
+
+%% Necessary parameters
+
+if operationMode == 2 % concentration dependent operation
+    temperature = interp1(temperatureRamp(1,:),temperatureRamp(2,:),concentration_mom);
+else % time dependent operation
+    temperature = interp1(temperatureRamp(1,:),temperatureRamp(2,:),t_mom);  
+end
+theta = (temperature-18.85)/7.859;
+solubility = 1000*(p4*theta^4 + p3*theta^3 + p2*theta^2 + p1*theta + p0);
+supersaturation = concentration_mom/solubility;
+G = 1e6*3600*k1*exp(-k2/(temperature+273.15))*(supersaturation-1)^(2/3)*log(supersaturation)^(1/6)*exp(-k3/(((temperature+273.15)^2)*log(supersaturation)));
+
+%% ODE system
+
+dm0dt = 0; % This is added to double check the simulation is correct
+dm1dt = G*m0;
+dm2dt = 2*G*m1;
+dm3dt = 3*G*m2;
+dcdt = -particleDensity*shapeFactor*dm3dt;
+
+ddt = [dm0dt; dm1dt; dm2dt; dm3dt; dcdt];
+end
