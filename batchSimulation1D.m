0a2292ad6f479e8796664fa98609733b6cc1a02d
Mohammed Alsubeihi <mohammed.alsubeihi30@gmail.com>
2023-03-07 02:13:44 +0000

    added additive dependency

--- batchSimulation1D.m
+++ batchSimulation1D.m
@@ -9,6 +9,7 @@
 % Last modified:
 % - 2023/02/24, MA: initial creation
 % - 2023/03/02, MA: added dialogue boxes to gather user input
+% - 2023/03/06, MA: added additive dependency
 %
 % Purpose: to provide a framework for different simulations of a 1D batch
 % crystallization model.
@@ -25,6 +26,9 @@
 % Research 41. https://doi.org/10.1021/ie010680u
 % (4) Ramkrishna, D., 2000. Population balances : theory and applications
 % to particulate systems in engineering. Academic Press.
+% (5) Vetter, T., Mazzotti, M., Brozio, J., 2011. Slowing the growth rate
+% of ibuprofen crystals using the polymeric additive pluronic F127. Crystal
+% Growth and Design 11. https://doi.org/10.1021/cg200352u
 %
 % Input arguments
 % 
@@ -78,7 +82,7 @@ m3i=trapz(L.^3.*initialPSD);
 y0 = [m0i m1i m2i m3i initialConcentration];
 
 % prompt user for operation mode
-operationMode = listdlg("ListString",{'constant temperature (crash cooling)','constant supersaturation'},"PromptString",'Please select the required simulation',"SelectionMode","single");
+operationMode = listdlg("ListString",{'constant temperature (crash cooling)','constant supersaturation','additive crash cooling'},"PromptString",'Please select the required simulation',"SelectionMode","single");
 switch operationMode
 %% Constant temperature comparison
 
@@ -223,7 +227,7 @@ switch operationMode
             'Initial PSD (constant temperature)', ...
             'Final PSD (constant temperature')
 
-%% Constant supersaturation (growth only)
+%% Constant supersaturation
     case 2
         % create dialogue box to get constant supersaturation from user
         prompt = {'Constant supersaturation','Final temperature (equilibrium)'};
@@ -327,8 +331,121 @@ switch operationMode
         ylabel({'Average length', ['[' char(181) ']']})
         legend('High resolution','Method of moments')
 
+         % PSD plots:
+        figure(3)
+        % PSD evolution comparison
+        plot(L,f(:,1),'linewidth',1.2)
+        hold on
+        plot(L,f(:,end),'linewidth',1.2)
+        
+        set(gca,'FontSize',18)  
+        title('PSD evolution comparison')
+        xlabel({'time' '[s]'})
+        ylabel({'Number density', '[μm^{-1} kg^{-1}]'})
+        legend('Initial PSD (starting from equilibrium)', ...
+            'Final PSD (starting from equilibrium)')
     % PSD plots:
-%% Custom temperature ramp
+%% T. Vetter growth rate/solubility (additive) constant temperature comparison
+% this section compares the generic growth rate model to the one used by T.
+% Vetter (5)
+    case 3
+        % prompt user for constant temperature and additive concentration
+        prompt = {['Please input the constant temperature at which crystallisation takes place [' char(176) 'C]'],'Please input the concentration of additive used [kg/kg]'};
+        dlgtitle = 'Constant temperature';
+        definput = {'25','0'};
+        answer = inputdlg(prompt,dlgtitle, [1 35],definput);
+        constantTemperature = str2double(answer(1));
+        additiveConcentration = str2double(answer(2));
+
+        % generate temperature ramp (starting from equilibrium)
+        temperatureRamp = [0 5 5.00001 simulationTime; T0 T0 constantTemperature ...
+            constantTemperature];
+
+        % array containing the solubility parameters for each additive
+        % concentration: (c; p4; p3; p2; p1; p0) 
+        solubilityParameters = [0 0.04 0.08; 3.594e-3 2.719e-3 2.408e-3; 9.957e-3 8.747e-3 9.19e-3; 1.377e-2 1.785e-2 2.039e-2; 2.841e-2 3.658e-2 4.265e-2; 4.171e-2 5.046e-2 6.055e-2];
+        p4 = interp1(solubilityParameters(1,:),solubilityParameters(2,:),additiveConcentration);
+        p3 = interp1(solubilityParameters(1,:),solubilityParameters(3,:),additiveConcentration);
+        p2 = interp1(solubilityParameters(1,:),solubilityParameters(4,:),additiveConcentration);
+        p1 = interp1(solubilityParameters(1,:),solubilityParameters(5,:),additiveConcentration);
+        p0 = interp1(solubilityParameters(1,:),solubilityParameters(6,:),additiveConcentration);
+
+        % array containing the growth rate parameters for each additive
+        % concentration: (c; k1; k2; k3)
+        growthParameters = [0 0.04 0.08; 0.1 9.7 0.15; 4e3 5.3e3 5.5e3; 1.48e4 2.04e4 1.8e4];
+        k1 = interp1(growthParameters(1,:), growthParameters(2,:),additiveConcentration);
+        k2 = interp1(growthParameters(1,:), growthParameters(3,:),additiveConcentration);
+        k3 = interp1(growthParameters(1,:), growthParameters(4,:),additiveConcentration);
+        
+        % high resolution solution:
+        [f, concentration, G, supersaturation, m3, t,...
+            temperature] = highRes1D_additive(dL, L, simulationTime, k1, k2, k3, p0, p1, p2, p3, p4, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, operationMode);
+
+        % method of moments solution:
+%         [t_mom, y] = ode23s(@(t_mom, y)mom_additive(t_mom, y, temperatureRamp, k1, k2, k3, p0, p1, p2, p3, p4, ...
+%             particleDensity, shapeFactor, operationMode), [0 simulationTime], y0);
+%         concentration_mom = y(:,5);
+
+% solubility curve plots
+        figure(1)
+        % plot of system starting at equilibrium then crash cooling
+        plot(temperature,concentration,'linewidth',1.6)
+        hold on
+        
+        % plot predicted by method of moments
+    
+%          plot([T0; temperature_mom],[initialConcentration; concentration_mom],'--','linewidth',1.2)
+        
+        % generate solubility curve for reference
+%         T = 0:0.1:70;
+%         solubility = 3.37*exp(0.0359*T);
+        
+        % plot of solubility curve to show driving force
+        %plot(T,solubility,'linewidth',1.2)
+        
+        set(gca,'FontSize',18)
+        title('Solubility curve')
+        xlabel({'Temperature', ['[' char(176) 'C]']})
+        ylabel({'Concentration' '[g kg^{-1}]'})
+        legend('High resolution', ...
+            'Method of moments ', ...
+            'Solubility curve')
+
+% concentration and average length plots
+        figure(2)
+        subplot(2,1,1)
+        
+        % concentration profile of system starting at equilibrium then crash
+        % cooling
+        plot(t,concentration,'linewidth',1.2)
+        hold on
+        
+        % concentration profile predicted by method of moments
+%         plot(t_mom,concentration_mom,'--','linewidth',1.2)
+        
+        set(gca,'FontSize',18)
+        title('Concentration profile')
+        xlabel({'time' '[s]'})
+        ylabel({'Concentration' '[g kg^{-1}]'})
+        legend('High resolution','Method of moments')
+        
+        % average length profile
+
+% PSD plots:
+        figure(3)
+        % PSD evolution comparison
+        plot(L,f(:,1),'linewidth',1.2)
+        hold on
+        plot(L,f(:,end),'linewidth',1.2)
+        
+        set(gca,'FontSize',18)  
+        title('PSD evolution comparison')
+        xlabel({'time' '[s]'})
+        ylabel({'Number density', '[μm^{-1} kg^{-1}]'})
+        legend('Initial PSD (starting from equilibrium)', ...
+            'Final PSD (starting from equilibrium)')
+    %% Custom temperature ramp
 % elseif operationMode == 3
 % prompt user for temperature ramp
 % temperatureRamp = input(['\nProvide the desired temperature ramp as a matrix' ...
