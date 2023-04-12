57029c8d0a4a5e7a275c3047365860df8caebad3
Mohammed Alsubeihi <mohammed.alsubeihi30@gmail.com>
2023-04-12 23:55:59 +0100

    changed kinetics + added average length support

--- mom_additive.m
+++ mom_additive.m
@@ -9,6 +9,7 @@
 % Last modified:
 % - 2022/03/06, MA: initial creation
 % - 2023/03/07, MA: Fixed growth rate
+% - 2023/04/30, MA: changed kinetics + added average length support
 %
 % Purpose: Defines a function handle containing the ode system
 % corresponding to the one-dimensional method of moments.This function
@@ -46,27 +47,29 @@
 % moments
 % 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function ddt = mom_additive(t_mom, y, temperatureRamp, k1, k2, k3, p0, p1, p2, p3, p4, particleDensity, ...
-    shapeFactor, operationMode)
+function ddt = mom_additive(t_mom, y, temperatureRamp, kg1, kg2, kg3, kd1, kd2, particleDensity, ...
+    shapeFactor, growthFactor, solubilityFactor)
 %% Store variables to be solved
 
 m0 = y(1);
 m1 = y(2);
 m2 = y(3);
 m3 = y(4);
-concentration_mom = y(5);
+m4 = y(5);
+m5 = y(6);
+concentration_mom = y(7);
 
 %% Necessary parameters
-
-if operationMode == 2 % concentration dependent operation
-    temperature = interp1(temperatureRamp(1,:),temperatureRamp(2,:),concentration_mom);
-else % time dependent operation
-    temperature = interp1(temperatureRamp(1,:),temperatureRamp(2,:),t_mom);  
-end
-theta = (temperature-18.85)/7.859;
-solubility = 1000*(p4*theta^4 + p3*theta^3 + p2*theta^2 + p1*theta + p0);
+temperature = interp1(temperatureRamp(1,:),temperatureRamp(2,:),t_mom);
+solubility = solubilityFactor*3.37*exp(0.036*temperature);
 supersaturation = concentration_mom/solubility;
-G = 1e6*k1*exp(-k2/(temperature+273.15))*(supersaturation-1)^(2/3)*log(supersaturation)^(1/6)*exp(-k3/(((temperature+273.15)^2)*log(supersaturation))); %[um/s]
+if supersaturation > 1
+    G = growthFactor*kg1*exp(-kg2/(temperature+273.15))*(supersaturation-1)^kg3; %[um/h]
+elseif supersaturation < 1
+    G = kd1*exp(-kd2/(temperature+273.15))*(supersaturation-1); %[um/h]
+else
+    G = 0;
+end
 
 %% ODE system
 
@@ -74,7 +77,9 @@ dm0dt = 0; % This is added to double check the simulation is correct
 dm1dt = G*m0;
 dm2dt = 2*G*m1;
 dm3dt = 3*G*m2;
+dm4dt = 4*G*m3;
+dm5dt = 5*G*m4;
 dcdt = -particleDensity*shapeFactor*dm3dt;
 
-ddt = [dm0dt; dm1dt; dm2dt; dm3dt; dcdt];
+ddt = [dm0dt; dm1dt; dm2dt; dm3dt; dm4dt; dm5dt; dcdt];
 end
