51c90fc945be4538586201f3b6d6f7ba9819fe33
Mohammed Alsubeihi <mohammed.alsubeihi30@gmail.com>
2023-04-12 23:47:47 +0100

    -added 1D numerical scheme comparison -improved plots + added auto-plot saving

--- batchSimulation1D_additive.m
+++ batchSimulation1D_additive.m
@@ -1,19 +1,21 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % The University of Manchester, United Kingdom
-% 
+%
 % Project: MEng Dissertation
 % Year: 2023
 % MATLAB: R2022b
 % Author(s): Mohammed Alsubeihi (MA)
-% 
+%
 % Last modified:
 % - 2023/03/07, MA: initial creation
 % - 2023/03/08, MA: fixed trapz function
+% - 2023/04/01, MA: added 1D numerical scheme comparison
+% - 2023/04/02, MA: improved plots + added automatic plot saving
 %
 % Purpose: to provide a framework for different simulations of a 1D batch
 % crystallization model in the presence of additive.
 %
-% References: 
+% References:
 % (1) LeVeque, R.J., 2002. Finite Volume Methods for Hyperbolic Problems,
 % Cambridge Texts in Applied Mathematics. Cambridge University Press,
 % Cambridge. https://doi.org/10.1017/CBO9780511791253
@@ -30,7 +32,7 @@
 % Growth and Design 11. https://doi.org/10.1021/cg200352u
 %
 % Input arguments
-% 
+%
 % Output arguments
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -38,38 +40,29 @@
 clear; clc; close all
 
 % create a dialogue box to gather essential parameters from user
-prompt = {'Initial concentration [g/kg]','additive concentration [kg/kg]s','shape factor', ['particle density [g/' char(181) 'm' char(179) ']'],'simulation time [h]', ['length step size [' char(181) 'm]']};
+prompt = {'Initial concentration [g/kg]','additive concentration [kg/kg]','shape factor', ['particle density [g/' char(181) 'm' char(179) ']'],'simulation time [h]', ['length step size [' char(181) 'm]']};
 dlgtitle = 'Essential parameters';
-definput = {'50','0','0.5236','1.1128e-12','9000','1'};
-essentialParameters = inputdlg(prompt,dlgtitle, [1 35],definput);
+definput = {'8','0','0.5236','1.1128e-12','50','1'};
+essentialParameters = inputdlg(prompt,dlgtitle, [1 35], definput);
 
 initialConcentration = str2double(essentialParameters(1)); % [g/kg]
 
 additiveConcentration = str2double(essentialParameters(2)); % [kg/kg]
 
-% array containing the solubility parameters for each additive
-% concentration: (c; p4; p3; p2; p1; p0) 
-solubilityParameters = [0 0.04 0.08; 3.594e-3 2.719e-3 2.408e-3;
-    9.957e-3 8.747e-3 9.19e-3;
-    1.377e-2 1.785e-2 2.039e-2;
-    2.841e-2 3.658e-2 4.265e-2;
-    4.171e-2 5.046e-2 6.055e-2];
-p4 = interp1(solubilityParameters(1,:),solubilityParameters(2,:),additiveConcentration);
-p3 = interp1(solubilityParameters(1,:),solubilityParameters(3,:),additiveConcentration);
-p2 = interp1(solubilityParameters(1,:),solubilityParameters(4,:),additiveConcentration);
-p1 = interp1(solubilityParameters(1,:),solubilityParameters(5,:),additiveConcentration);
-p0 = interp1(solubilityParameters(1,:),solubilityParameters(6,:),additiveConcentration);
+% additive effect (NEEDS UPDATE)
+solubilityFactor = 1;
+growthFactor = 1;
 
-        % array containing the growth rate parameters for each additive
-        % concentration: (c; k1; k2; k3)
-growthParameters = [0 0.04 0.08; 0.1 9.7 15; 4e3 5.3e3 5.5e3; 1.48e4 2.04e4 1.8e4];
-k1 = interp1(growthParameters(1,:), growthParameters(2,:),additiveConcentration);
-k2 = interp1(growthParameters(1,:), growthParameters(3,:),additiveConcentration);
-k3 = interp1(growthParameters(1,:), growthParameters(4,:),additiveConcentration);
+% kinetics
+kg1 = 3600*2700;
+kg2 = 2400;
+kg3 = 3.7;
+kd1 = 3600*1.636e6/1000;
+kd2 = 3572;
 
 % equilibrium temperature at initial concentration (starting point of
 % any simulation)
-T0 = 7.859*fzero(@(theta) 1000*(p4*theta^4 + p3*theta^3 + p2*theta^2 + p1*theta + p0) - initialConcentration,1) + 18.85;
+T0 = (1/0.036)*log(initialConcentration/(3.37*solubilityFactor));
 
 shapeFactor = str2double(essentialParameters(3)); % assuming cuboidal particles
 particleDensity = str2double(essentialParameters(4)); % [g/um3]
@@ -83,10 +76,10 @@ L = 1:dL:1000; % [um]
 choicePSD = listdlg('ListString',{'Gaussian','Pulse'},'PromptString','Please select an inital particle size distribution','SelectionMode','single');
 % use choice to generate PSD
 if choicePSD == 1
-    initialPSD = 226458*normpdf(L,300,20);
+    initialPSD = 1e5*normpdf(L,300,20);
 elseif choicePSD == 2
     initialPSD = zeros(1,length(L));
-    initialPSD(100:200)=1e3;
+    initialPSD(250:350)=1e3;
 end
 
 % calculate initial moments using intergation
@@ -94,62 +87,69 @@ m0i=trapz(L,initialPSD);
 m1i=trapz(L,L.*initialPSD);
 m2i=trapz(L,L.^2.*initialPSD);
 m3i=trapz(L,L.^3.*initialPSD);
+m4i = trapz(L,L.^4.*initialPSD);
+m5i = trapz(L,L.^5.*initialPSD);
 
 % define vector containing initial conditions for method of moments
-y0 = [m0i m1i m2i m3i initialConcentration];
+y0 = [m0i m1i m2i m3i m4i m5i initialConcentration];
 
 % prompt user for operation mode
-operationMode = listdlg("ListString",{'constant temperature (crash cooling)'},"PromptString",'Please select the required simulation',"SelectionMode","single");
-switch operationMode
-%% T. Vetter growth rate/solubility (additive)(5) constant temperature comparison
+simulationMode = listdlg("ListString",{'constant temperature (crash cooling)','Numerical scheme comparison'},"PromptString",'Please select the required simulation',"SelectionMode","single");
+switch simulationMode
+    %% T. Vetter growth rate/solubility (additive)(5) constant temperature comparison
     case 1
-        % prompt user for constant temperature and additive concentration
+        % prompt user for constant temperature
         prompt = {['Please input the constant temperature at which crystallisation takes place [' char(176) 'C]']};
         dlgtitle = 'Constant temperature';
-        definput = {'15'};
+        definput = {'10'};
         answer = inputdlg(prompt,dlgtitle, [1 35],definput);
         constantTemperature = str2double(answer(1));
 
         % generate temperature ramp (starting from equilibrium)
-        temperatureRamp = [0 900 900.01 simulationTime; T0 T0 constantTemperature ...
+        temperatureRamp = [0 1 1.001 simulationTime; T0 T0 constantTemperature ...
             constantTemperature];
-        
+
         % high resolution solution:
         [f, concentration, G, supersaturation, m3, t,...
-            temperature] = highRes1D_additive(dL, L, simulationTime, k1, k2, k3, p0, p1, p2, p3, p4, shapeFactor,...
-            temperatureRamp, particleDensity, initialConcentration, initialPSD, operationMode);
+            temperature] = highRes1D_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
         %calculate average length for comparison to method of moments
         m0 = trapz(L,f);
         m1 = trapz(L,L'.*f);
-        averageLength = m1./m0;
+        m4 = trapz(L,L'.^4.*f);
+        m5 = trapz(L,L'.^5.*f);
+        averageLength = m4./m3;
+        stdLength = sqrt(abs(m5./m3-averageLength.^2));
 
         % method of moments solution:
-        [t_mom, y] = ode15s(@(t_mom, y)mom_additive(t_mom, y, temperatureRamp, k1, k2, k3, p0, p1, p2, p3, p4, ...
-            particleDensity, shapeFactor, operationMode), [0 simulationTime], y0);
-        concentration_mom = y(:,5);
+        options = odeset('RelTol',1e-8,'AbsTol',1e-10);
+        [t_mom, y] = ode113(@(t_mom, y)mom_additive(t_mom, y, temperatureRamp, kg1, kg2, kg3, kd1, kd2, particleDensity, ...
+            shapeFactor, growthFactor, solubilityFactor), [0 simulationTime], y0,options);
+        concentration_mom = y(:,7);
         % extract temperature profile predicted by method of moments
         temperature_mom = interp1(temperatureRamp(1,:),temperatureRamp(2,:),t_mom);
         % average length predicted by method of moments
-        averageLength_mom  = y(:,2)./y(:,1);
+        averageLength_mom  = y(:,5)./y(:,4);
+        stdLength_mom = sqrt(abs(y(:,6)./y(:,4)-averageLength_mom.^2));
 
-% solubility curve plots
+        % solubility curve plots:
         figure(1)
         % plot of system starting at equilibrium then crash cooling
         plot(temperature,concentration,'linewidth',1.6)
         hold on
-        
+
         % plot predicted by method of moments
-    
-         plot([T0; temperature_mom],[initialConcentration; concentration_mom],'--','linewidth',1.2)
-        
+
+        plot(temperature_mom,concentration_mom,'--','linewidth',1.2)
+
         % generate solubility curve for reference
-        T = 5:0.1:35;
-        theta = (T-18.85)/7.859;
-        solubility = 1000*(p4*theta.^4 + p3*theta.^3 + p2*theta.^2 + p1*theta + p0);
-        
+        T = 0:0.1:45;
+
+        solubility = solubilityFactor*3.37*exp(0.036*T);
+
         % plot of solubility curve to show driving force
         plot(T,solubility,'linewidth',1.2)
-        
+
         set(gca,'FontSize',18)
         title('Solubility curve')
         xlabel({'Temperature', ['[' char(176) 'C]']})
@@ -158,50 +158,235 @@ switch operationMode
             'Method of moments ', ...
             'Solubility curve')
 
-% concentration and average length plots
+        % concentration and average length plots:
         figure(2)
         subplot(2,1,1)
-        
+
         % concentration profile of system starting at equilibrium then crash
         % cooling
         plot(t,concentration,'linewidth',1.2)
         hold on
-        
+
         % concentration profile predicted by method of moments
-         plot(t_mom,concentration_mom,'--','linewidth',1.2)
-        
+        plot(t_mom,concentration_mom,'--','linewidth',1.2)
+
         set(gca,'FontSize',18)
         title('Concentration profile')
-        xlabel({'time' '[s]'})
+        xlabel({'time' '[h]'})
         ylabel({'Concentration' '[g kg^{-1}]'})
         legend('High resolution','Method of moments')
-        
+
         % average length profile
         subplot(2,1,2)
         % average length profile of system starting at equilibrium then crash
         % cooling:
         plot(t,averageLength,'linewidth',1.2)
         hold on
-        
+
         % average length profile predicted by method of moments
         plot(t_mom,averageLength_mom,'--','linewidth',1.2)
-    
-        set(gca,'FontSize',18)  
+
+        set(gca,'FontSize',18)
         title('Average length profile')
-        xlabel({'time' '[s]'})
+        xlabel({'time' '[h]'})
         ylabel({'Average length', ['[' char(181) 'm]']})
         legend('High resolution (starting from equilibrium)','Method of moments')
-% PSD plots:
+        % PSD plots:
         figure(3)
         % PSD evolution comparison
         plot(L,f(:,1),'linewidth',1.2)
         hold on
         plot(L,f(:,end),'linewidth',1.2)
-        
-        set(gca,'FontSize',18)  
-        title('PSD evolution comparison')
+
+        set(gca,'FontSize',18)
+        title('PSD evolution')
         xlabel({'Length' ['[' char(181) 'm]']})
         ylabel({'Number density', '[μm^{-1} kg^{-1}]'})
         legend('Initial PSD (starting from equilibrium)', ...
             'Final PSD (starting from equilibrium)')
+        % standard deviation plots:
+        figure(4)
+        plot(t,stdLength,'LineWidth',1.2)
+        hold on
+        plot(t_mom,stdLength_mom,'--','LineWidth',1.2)
+
+        set(gca,'FontSize',18)
+        title('Standard deviation profile')
+        xlabel({'time', '[h]'})
+        ylabel({'Number density', '[μm^{-1} kg^{-1}]'})
+        legend('High resolution', ...
+            'Method of moments')
+        % moments plots
+        figure(5)
+        subplot(3,2,1)
+        plot(t,m0,'LineWidth',1.2)
+        hold on
+        plot(t_mom,y(:,1),'--','LineWidth',1.2)
+        set(gca,'FontSize',18)
+        title('m0 profile')
+
+        subplot(3,2,2)
+        plot(t,m1,'LineWidth',1.2)
+        hold on
+        plot(t_mom,y(:,2),'--','LineWidth',1.2)
+        set(gca,'FontSize',18)
+        title('m1 profile')
+
+        subplot(3,2,3)
+        plot(t,m4,'LineWidth',1.2)
+        hold on
+        plot(t_mom,y(:,5),'--','LineWidth',1.2)
+        set(gca,'FontSize',18)
+        title('m4 profile')
+
+        subplot(3,2,4)
+        plot(t,m5,'LineWidth',1.2)
+        hold on
+        plot(t_mom,y(:,6),'--','LineWidth',1.2)
+        set(gca,'FontSize',18)
+        title('m5 profile')
+
+        subplot(3,2,[5 6])
+        plot(t,m3,'LineWidth',1.2)
+        hold on
+        plot(t_mom,y(:,4),'--','LineWidth',1.2)
+        set(gca,'FontSize',18)
+        title('m3 profile')
+
+        %% Numerical schemes comparison
+    case 2
+% create a folder with the current date and time:
+        timeDate = string(datetime('now','TimeZone','local','Format', 'dd-MM-yyyy HH.mm.ss'));
+        mkdir('C:\Users\moham\OneDrive - The University of Manchester\Year 4 modules\Dissertation (CHEN40100)\Figures\Batch1D_additive\SchemeComparison', timeDate)
+% prompt user for constant temperature
+        prompt = {['Please input the constant temperature at which crystallisation takes place [' char(176) 'C]']};
+        dlgtitle = 'Constant temperature';
+        definput = {'-10'};
+        answer = inputdlg(prompt,dlgtitle, [1 35],definput);
+        constantTemperature = str2double(answer(1));
+
+        % generate temperature ramp (starting from equilibrium)
+        temperatureRamp = [0 simulationTime; constantTemperature ...
+            constantTemperature];
+% upwind comparison:
+        % Gaussian:
+        initialPSD = 1e5*normpdf(L,300,20);
+
+% high resolution solution:
+        [f, ~, ~, ~, m3, t] = highRes1D_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
+        %calculate average length for comparison to method of moments
+        m0 = trapz(L,f);
+        m1 = trapz(L,L'.*f);
+        m4 = trapz(L,L'.^4.*f);
+        m5 = trapz(L,L'.^5.*f);
+        averageLength = m4./m3;
+        stdLength = sqrt(abs(m5./m3-averageLength.^2));
+
+% upwind solution:
+        [f_upwind, ~, ~, ~, m3_upwind, t_upwind] = upwind_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
+        %calculate average length for comparison to method of moments
+        m0_upwind = trapz(L,f);
+        m1_upwind = trapz(L,L'.*f);
+        m4_upwind = trapz(L,L'.^4.*f);
+        m5_upwind = trapz(L,L'.^5.*f);
+        averageLength_upwind = m4./m3_upwind;
+        stdLength_upwind = sqrt(abs(m5./m3_upwind-averageLength_upwind.^2));
+
+% standard deviation comparison:
+% Lax-Wendroff solution (other standard deviations already available):
+        [~, ~, ~, ~, m3_LW, t_LW] = LaxWendroff_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
+        m0_LW = trapz(L,f);
+        m1_LW = trapz(L,L'.*f);
+        m4_LW = trapz(L,L'.^4.*f);
+        m5_LW = trapz(L,L'.^5.*f);
+        averageLength_LW = m4./m3_LW;
+        stdLength_LW = sqrt(abs(m5./m3_LW-averageLength_LW.^2));
+
+% standard deviation plots:
+        figure(1)
+        % fraction of simulation time to stop plotting:
+        upperlimit = simulationTime; 
+        lowerlimit = 0.1*simulationTime;
+
+        plot(t,stdLength/stdLength(1),'LineWidth',1.5)
+        hold on
+
+        plot(t_upwind,stdLength_upwind/stdLength(1),'-.','LineWidth',1.5)
+
+        plot(t_LW,stdLength_LW/stdLength(1),'--','LineWidth',1.5)
+
+        set(gca,'FontSize',8,'XTick',[],'fontname','times')
+        fig = gcf;
+        fig.Position(3:4)=[312 312];
+        colororder(['#000000';'#00FFFF';'#FFD700'])
+        legend('High resolution scheme', 'Upwind scheme', 'Lax-Wendroff scheme','Location','northwest')
+        xlabel({'{\it t}','[h]'})
+        ylabel({'\sigma_{model}/\sigma_{actual}','[-]'})
+        xlim([lowerlimit upperlimit])
+        fileName= 'Gaussian standard deviation scheme comaprison.pdf';
+        fpath = strcat('C:\Users\moham\OneDrive - The University of Manchester\Year 4 modules\Dissertation (CHEN40100)\Figures\Batch1D_additive\SchemeComparison\', timeDate);
+        exportgraphics(gca, fullfile(fpath, fileName), 'ContentType','vector');
+        clear upperlimit lower limit
+
+% upwind PSD comparison plots:
+        figure(2)
+        plot(L, initialPSD,':','LineWidth',1.5) % initial
+        hold on
+
+        plot(L,f(:,end), 'LineWidth',1.5) % high resolution
+
+        plot(L, f_upwind(:,end),'--', 'LineWidth',1.5) % upwind
+
+        set(gca,'FontSize',8,'FontName','times')
+        fig = gcf;
+        fig.Position(3:4)=[312 312];
+        colororder(["#000000";"#000000";"#FFD700"])
+        legend('Initial condition', 'High resolution solution', 'Upwind solution','Location','north')
+        xlabel({'L','[\mum]'})
+        ylabel({'f','[kg^{-1}\mum^{-1}]'})
+
+% Lax-Wendroff comparison:
+        % pulse:
+        initialPSD = zeros(1,length(L));
+        initialPSD(250:350)=1e3; 
+
+% high resolution solution:
+        f = highRes1D_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
+
+% Lax-Wendroff solution:
+        f_LW = LaxWendroff_additiveTD(dL, L, simulationTime, kg1, kg2, kg3, kd1, kd2, shapeFactor,...
+            temperatureRamp, particleDensity, initialConcentration, initialPSD, growthFactor, solubilityFactor);
+        
+% Lax-Wendroff PSD comparison plots:
+        figure(3)
+        plot(L, initialPSD,':','LineWidth',1.5) % initial
+        hold on
+
+        plot(L,f(:,end), 'LineWidth',1.5) % high resolution
+
+        plot(L, f_LW(:,end),'--', 'LineWidth',1.5) % Lax-Wendroff
+
+        set(gca,'FontSize',8,'fontname','times')
+        fig = gcf;
+        fig.Position(3:4)=[312 312];
+        colororder(['#000000';'#000000';'#00A300'])
+        legend('Initial condition', 'High resolution solution', 'Lax-Wendroff solution','Location','north')
+        xlabel({'{\it L}',['[' char(181) 'm]']})
+        ylabel({'{\it f}',['[kg^{-1}' char(181) 'm^{-1}]']})
+        fileName='Lax-Wendroff PSD comparison.pdf';
+        fpath = strcat('C:\Users\moham\OneDrive - The University of Manchester\Year 4 modules\Dissertation (CHEN40100)\Figures\Batch1D_additive\SchemeComparison\', timeDate);
+        exportgraphics(gca, fullfile(fpath, fileName), 'ContentType','vector');
+
+% store operating conditions etc in a matrix to be exported as a
+% text file
+        namesCell = {'Initial Concentration'; 'Additive concentration';'Shape factor'; 'Particle density'; 'Simulation time';'Bin size';'Temperature'};
+        unitsCell = {'[g/kg]';'[kg/kg]'; '[-]';['[g/' char(181) 'm' char(179) ']'];'[h]';['[' char(181) 'm]'];['[' char(176) 'C]']};
+        parametersCell = cat(1, [namesCell, [essentialParameters(:); constantTemperature], unitsCell]);
+        fileName='Numerical schemes comparison parameters';
+        fpath = strcat('C:\Users\moham\OneDrive - The University of Manchester\Year 4 modules\Dissertation (CHEN40100)\Figures\Batch1D_additive\SchemeComparison\', timeDate);
+        writecell(parametersCell,fullfile(fpath,fileName))
 end
