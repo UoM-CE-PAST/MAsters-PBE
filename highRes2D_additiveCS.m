34c550810ca8a3b0f0519d436a16ce692fbf35b0
Mohammed Alsubeihi <mohammed.alsubeihi30@gmail.com>
2023-05-05 06:46:18 +0100

    small improvements

--- highRes2D_additiveCS.m
+++ highRes2D_additiveCS.m
@@ -8,6 +8,7 @@
 %
 % Last modified:
 % - 2023/03/26, MA: initial creation
+% - 2023/04/30, MA: small improvements
 %
 %
 % Purpose: Implements a high resolution finite volume method (with van Leer
@@ -41,7 +42,7 @@
 % Output arguments
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function [f, concentration, G1, G2, supersaturation, m00, m31, m22, m21, t, temperature] = highRes2D_additiveCS(dL1,dL2, L1, L2, simulationTime, kg11, kg12, kg13, kg21, kg22, kg23, kd11, kd12, kd21, kd22, shapeFactor, particleDensity, initialConcentration, initialPSD, yieldFactor, maxCycleNumber, supersaturationLimits,growthFactor,solubilityFactor)
+function [f, concentration, G1, G2, supersaturation, m00, m31, m22, m21, m41, m23, t, temperature] = highRes2D_additiveCS(dL1,dL2, L1, L2, simulationTime, kg11, kg12, kg13, kg21, kg22, kg23, kd11, kd12, kd21, kd22, shapeFactor, particleDensity, initialConcentration, initialPSD, yieldFactor, maxCycleNumber, supersaturationLimits,growthFactor,solubilityFactor)
 
 %initialise variables (overspecify then slice off later)
 t = zeros(1,50000);
@@ -49,6 +50,9 @@ m00 = zeros(1,length(t));
 m31 = zeros(1,length(t));
 m22 = zeros(1,length(t));
 m21 = zeros(1,length(t));
+m41 = zeros(1,length(t));
+m23 = zeros(1,length(t));
+
 concentration = zeros(1,length(t));
 temperature = zeros(1,length(t));
 supersaturation = zeros(1,length(t));
@@ -63,6 +67,8 @@ m00(1) = sum(initialPSD,'all')*dL1*dL2;
 m31(1) = sum(L1.^3.*L2'.*initialPSD,'all')*dL1*dL2;
 m22(1) = sum(L1.^2.*L2'.^2.*initialPSD,'all')*dL1*dL2;
 m21(1) = sum(L1.^2.*L2'.*initialPSD,'all')*dL1*dL2;
+m41(1) = sum(L1.^4.*L2'.*initialPSD,'all')*dL1*dL2;
+m23(1) = sum(L1.^2.*L2'.^3.*initialPSD,'all')*dL1*dL2;
 concentration(1) = initialConcentration;
 Tmax = (1/0.036)*log(initialConcentration/(3.37*solubilityFactor));
 temperature(1) = Tmax;
@@ -100,18 +106,22 @@ CourantNumber = 1;
 % set reference time and time index
 n=1;
 cycleNumber = 0;
-
+relativeError = 0;
+relativeTolerance = 0.5;
+stepLimiter = 0.1;
 
 % if system starts at equilibrium (i.e. G=0) then ensure system doesn't
 % change until driving force is non-zero
 while temperature(n) == Tmax || supersaturation(n) == 1
-    t(n+1) = 0; % for constant supersaturation operation,
+    t(n+1) = 0.00001; % for constant supersaturation operation,
     % immediately start simulation
     f = initialPSD;
     m00(n+1) = sum(initialPSD,'all')*dL1*dL2;
     m31(n+1) = sum(L1.^3.*L2'.*initialPSD,'all')*dL1*dL2;
     m22(n+1) = sum(L1.^2.*L2'.^2.*initialPSD,'all')*dL1*dL2;
     m21(n+1) = sum(L1.^2.*L2'.*initialPSD,'all')*dL1*dL2;
+    m41(n+1) = sum(L1.^4.*L2'.*initialPSD,'all')*dL1*dL2;
+    m23(n+1) = sum(L1.^2.*L2'.^3.*initialPSD,'all')*dL1*dL2;
 
     concentration(n+1) = initialConcentration;
 
@@ -143,6 +153,10 @@ while t(n)<simulationTime
         % Growth
         % Calculate stable time step using available values
         dt=CourantNumber/(G1(n)/dL1 + G2(n)/dL2);
+
+        if relativeError >= relativeTolerance
+            dt = stepLimiter*dt;
+        end
 
         %Check if the max stable time step will exceed time range
         if simulationTime-t(n)<=dt
@@ -207,6 +221,8 @@ while t(n)<simulationTime
         m31(n+1) = sum(L1.^3.*L2'.*f,'all')*dL1*dL2;
         m22(n+1) = sum(L1.^2.*L2'.^2.*f,'all')*dL1*dL2;
         m21(n+1)=sum(L1.^2.*L2'.*f,'all')*dL1*dL2;
+        m41(n+1) = sum(L1.^4.*L2'.*f,'all')*dL1*dL2;
+        m23(n+1) = sum(L1.^2.*L2'.^3.*f,'all')*dL1*dL2;
         concentration(n+1)=concentration(n)-particleDensity*shapeFactor*(m21(n+1)-m21(n));
 
         % determine temperature based on supersaturation, within limits
@@ -214,7 +230,8 @@ while t(n)<simulationTime
         if temperature(n+1) < Tmin
             temperature(n+1) = Tmin;
         end
-        if concentration(n+1) <= 1.1*cmin && cycleNumber < maxCycleNumber
+
+        if concentration(n) <= (cmin + 0.75*(cmax-cmin)) && cycleNumber < maxCycleNumber
             temperature(n+1) = (1/0.036)*log(concentration(n+1)/(3.37*solubilityFactor*Smin));
             if temperature(n+1) > Tmax
                 temperature(n+1) = Tmax;
@@ -230,13 +247,22 @@ while t(n)<simulationTime
             G1(n+1) = kd11*exp(-kd12/(temperature(n+1)+273.15))*(supersaturation(n+1)-1);
             G2(n+1) = kd21*exp(-kd22/(temperature(n+1)+273.15))*(supersaturation(n+1)-1);
         end
-        f_old = f;
-        % Increase time counter
-        n=n+1;
+
+        relativeError = 100*abs(concentration(n+1)-concentration(n))/concentration(n+1);
+
+        if relativeError < relativeTolerance
+            f_old = f;
+            % Increase time counter
+            n=n+1;
+        end
     elseif supersaturation(n) <= 1
         % Dissolution
         % Courant number is now negative
         dt=-CourantNumber/(G1(n)/dL1 + G2(n)/dL2);
+
+        if relativeError >= relativeTolerance
+            dt = stepLimiter*dt;
+        end
 
         %Check if the max stable time step will exceed time range
         if simulationTime-t(n)<=dt
@@ -245,6 +271,7 @@ while t(n)<simulationTime
         else
             t(n+1)=t(n)+dt;
         end
+        
 
         % Calculate the PSD at the new time using the high resolution
         % method: for 1, 2 & 3, the smoothness is determined and the
@@ -257,14 +284,14 @@ while t(n)<simulationTime
         % spatial domain. The ghost celss are assumed to be 0.
 
         fluxLimiter(j,end) = 1;
-        fluxLimiter(j,end) = 1;
+        fluxLimiter(j,end-1) = 1;
 
         f_mid(j,end)=f_old(j,end)+dt/dL1*G1(n)*f_old(j,end)+0.5*dt/dL1*G1(n)*(1+dt/dL1*G1(n))*(fluxLimiter(j,end).*(-f_old(j,end))-fluxLimiter(j,end-1).*(f_old(j,end)-f_old(end-1)));
 
         %% 2-Interior volume
         smoothness(j,end-2:-1:1) = (f_old(j,end:-1:3)-f_old(j,end-1:-1:2)+eps)./(f_old(j,end-1:-1:2)-f_old(j,end-2:-1:1)+eps);
         fluxLimiter(j,end-2:-1:1) = (smoothness(j,end-2:-1:1)+abs(smoothness(j,end-2:-1:1)))./(1+abs(smoothness(j,end-2:-1:1)));
-        f_mid(j,end-1:-1:2) = f_old(j,end-1:-1:2) - dt/dL1*(G1(n)*f_old(j,end:-1:3)-G1(n)*f_old(j,end-1:-1:2)) + 0.5*dt/dL1*G1(n)*(1+dt/dL1*G1(n))*(fluxLimiter(j,end-2:-1:1).*(f_old(j,end:-1:3)-f_old(j,end-1:-1:2)) - fluxLimiter(j,end-1:-1:2).*(f_old(j,end-1:-1:2)-f_old(j,end-2:-1:1)));
+        f_mid(j,end-1:-1:2) = f_old(j,end-1:-1:2) - dt/dL1*(G1(n)*f_old(j,end:-1:3)-G1(n)*f_old(j,end-1:-1:2)) + 0.5*dt/dL1*G1(n)*(1+dt/dL1*G1(n))*(fluxLimiter(j,end-1:-1:2).*(f_old(j,end:-1:3)-f_old(j,end-1:-1:2)) - fluxLimiter(j,end-2:-1:1).*(f_old(j,end-1:-1:2)-f_old(j,end-2:-1:1)));
 
         %% 3-Outflow boundary
         % For dissolution, the outflow boundary is at the left of the
@@ -272,6 +299,7 @@ while t(n)<simulationTime
 
         % An outlet flux limiter is not required for the outflow boundary
         f_mid(j,1)=f_old(j,1)-dt/dL1*(G1(n)*f_old(j,2)-G1(n)*f_old(j,1))+0.5*dt/dL1*G1(n)*(1+dt/dL1*G1(n))*(fluxLimiter(j,1).*(f_old(j,2)-f_old(j,1)));
+        
         %% L2 sweeps
         i = 1:length(L1);
         %% 1-Inflow boundary
@@ -288,27 +316,29 @@ while t(n)<simulationTime
 
         smoothness(end-2:-1:1,i) = (f_mid(end:-1:3,i)-f_mid(end-1:-1:2,i)+eps)./(f_mid(end-1:-1:2,i)-f_mid(end-2:-1:1,i)+eps);
         fluxLimiter(end-2:-1:1,i) = (smoothness(end-2:-1:1,i)+abs(smoothness(end-2:-1:1,i)))./(1+abs(smoothness(end-2:-1:1,i)));
-        f(end-1:-1:2,i) = f_mid(end-1:-1:2,i) - dt/dL2*(G2(n)*f_mid(end:-1:3,i)-G2(n)*f_mid(end-1:-1:2,i)) + 0.5*dt/dL2*G2(n)*(1+dt/dL2*G2(n))*(fluxLimiter(end-2:-1:1,i).*(f_mid(end:-1:3,i)-f_mid(end-1:-1:2,i)) - fluxLimiter(end-1:-1:2,i).*(f_mid(end-1:-1:2,i)-f_mid(end-2:-1:1,i)));
+        f(end-1:-1:2,i) = f_mid(end-1:-1:2,i) - dt/dL2*(G2(n)*f_mid(end:-1:3,i) - G2(n)*f_mid(end-1:-1:2,i)) + 0.5*dt/dL2*G2(n)*(1+dt/dL2*G2(n))*(fluxLimiter(end-1:-1:2,i).*(f_mid(end:-1:3,i)-f_mid(end-1:-1:2,i)) - fluxLimiter(end-2:-1:1,i).*(f_mid(end-1:-1:2,i)-f_mid(end-2:-1:1,i)));
 
         %% 3-Outflow boundary
 
         % An outlet flux limiter is not required for the outflow boundary
 
-        f(1,i)=f_mid(1,i)-dt/dL2*(G2(n)*f_mid(2,i)-G2(n)*f_mid(1,i))+0.5*dt/dL2*G2(n)*(1+dt/dL2*G2(n))*(fluxLimiter(1,i).*(f_mid(2,i)-f_mid(1,i)));
+        f(1,i)=f_mid(1,i)-dt/dL2*(G2(n)*f_mid(2,i)-G2(n)*f_mid(1,i)) + 0.5*dt/dL2*G2(n)*(1+dt/dL2*G2(n))*(fluxLimiter(1,i).*(f_mid(2,i) - f_mid(1,i)));
 
         %% Use liquid phase mass balance to determine supersaturation at next time step
         m00(n+1) = sum(f,'all')*dL1*dL2;
         m31(n+1) = sum(L1.^3.*L2'.*f,'all')*dL1*dL2;
         m22(n+1) = sum(L1.^2.*L2'.^2.*f,'all')*dL1*dL2;
-        m21(n+1)=sum(L1.^2.*L2'.*f,'all')*dL1*dL2;
-        concentration(n+1)=concentration(n)-particleDensity*shapeFactor*(m21(n+1)-m21(n));
+        m21(n+1) = sum(L1.^2.*L2'.*f,'all')*dL1*dL2;
+        m41(n+1) = sum(L1.^4.*L2'.*f,'all')*dL1*dL2;
+        m23(n+1) = sum(L1.^2.*L2'.^3.*f,'all')*dL1*dL2;
+        concentration(n+1) = concentration(n) - particleDensity*shapeFactor*(m21(n+1)-m21(n));
 
         % determine temperature based on supersaturation, within limits
         temperature(n+1) = (1/0.036)*log(concentration(n+1)/(3.37*solubilityFactor*Smin));
         if temperature(n+1) > Tmax
             temperature(n+1) = Tmax;
         end
-        if m21(n+1) <= m21min
+        if concentration(n) >= 0.99*cmax
             temperature(n+1) = (1/0.036)*log(concentration(n+1)/(3.37*solubilityFactor*Smax));
             cycleNumber = cycleNumber + 1;
             if temperature(n+1) < Tmin
@@ -326,9 +356,14 @@ while t(n)<simulationTime
             G1(n+1) = kg11*exp(-kg12/(temperature(n+1)+273.15))*(supersaturation(n+1)-1)^kg13;
             G2(n+1) = growthFactor*kg21*exp(-kg22/(temperature(n+1)+273.15))*(supersaturation(n+1)-1)^kg23;
         end
-        f_old = f;
-        % Increase time counter
-        n=n+1;
+
+        relativeError = 100*abs(concentration(n+1)-concentration(n))/concentration(n+1);
+
+        if relativeError < relativeTolerance
+            f_old = f;
+            % Increase time counter
+            n=n+1;
+        end
     end
 end
 % slice off unnecessary elements
@@ -337,6 +372,8 @@ m00 = m00(1:n);
 m31 = m31(1:n);
 m22 = m22(1:n);
 m21 = m21(1:n);
+m41 = m41(1:n);
+m23 = m23(1:n);
 concentration = concentration(1:n);
 temperature = temperature(1:n);
 supersaturation = supersaturation(1:n);
